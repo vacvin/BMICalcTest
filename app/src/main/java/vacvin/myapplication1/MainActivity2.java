a52514b36bcf0c9caed98c6d2eaa25dcabeeff19
vince <vacvin@gmail.com>
2015-04-20 15:24:03 +0800

    Transfer data between Activities.

--- app/src/main/java/vacvin/myapplication1/MainActivity2.java
+++ app/src/main/java/vacvin/myapplication1/MainActivity2.java
@@ -1,9 +1,15 @@
 package vacvin.myapplication1;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+//import java.text.DecimalFormat;
 
 
 public class MainActivity2 extends ActionBarActivity {
@@ -12,8 +18,42 @@ public class MainActivity2 extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activity2);
+        initViews();
+        showResults();
+        setListeners();
     }
 
+    private Button btn_back;
+    private TextView show_result;
+
+    private void initViews(){
+        btn_back = (Button)findViewById(R.id.back);
+        show_result =(TextView)findViewById(R.id.result);
+    }
+
+    private void showResults(){
+        Bundle bundle = this.getIntent().getExtras();
+        String result = bundle.getString("result");
+        show_result.setText(result);
+    }
+
+    private  void setListeners(){
+        btn_back.setOnClickListener(goback);
+    }
+
+    private View.OnClickListener goback = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            //back
+            Bundle bundle = new Bundle();
+            Intent intent = new Intent();
+
+            bundle.putString("BMI", show_result.getText().toString());
+            intent.putExtras(bundle);
+            setResult(RESULT_OK, intent);
+            MainActivity2.this.finish();
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
